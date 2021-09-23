c613f854e3baa79057fc062b5c1cd9c1e0180b02
Makenzie Adams <adamsm.realtor@gmail.com>
2021-09-23 14:02:23 -0400

    Added photo of me and Cash

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "";
 document.querySelector("body").appendChild(h2);
 
 const h3 = document.createElement("h3");
-h3.textContent = "This is my first website! Welcome!";
+h3.textContent = "";
 document.querySelector("body").appendChild(h3);
